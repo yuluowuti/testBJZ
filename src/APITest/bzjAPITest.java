eaef36f52a1d9dc58760adce8cb2d6bb3035a21f
yuluowuti <18192496382@163.com>
2018-08-23 09:57:51 +0800

    12

--- src/APITest/bzjAPITest.java
+++ src/APITest/bzjAPITest.java
@@ -26,4 +26,16 @@ public class bzjAPITest {
         ValidatableResponse response = given().headers(header).queryParams(params).and().get(url).then();
         System.out.println(response.extract().body().asString());
     }
+
+    @Test
+    public void test2() throws IOException {
+        String url = Host + "/v1/init/gui";
+        HashMap params =new HashMap();
+        params.put("version","v2");
+        params.put("module","homepage");
+        AddSign.sign(params);
+
+        ValidatableResponse response = given().headers(header).queryParams(params).and().get(url).then();
+        System.out.println(response.extract().body().asString());
+    }
 }
